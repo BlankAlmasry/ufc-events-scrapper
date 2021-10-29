38affe85b23c78713730640a3348bc6452a4cbe4
blankalmasry <blankhussien@gmail.com>
2021-10-29 19:00:27 +0200

    changed function names, and added comments

--- main.py
+++ main.py
@@ -6,33 +6,39 @@ from bs4 import BeautifulSoup
 base = "https://www.ufc.com"
 
 if __name__ == "__main__":
-    def iterate_through_ufc_events(next_event="/event/ufc-1"):
+    def recurse_through_ufc_events(next_event="/event/ufc-1"):
         page = requests.get(base + next_event)
         soup = BeautifulSoup(page.content, "html.parser")
+
         timestamp = int(soup.find('div', class_='tz-change-inner')['data-timestamp'])
+        # don't scrape the future event
         if timestamp > current_time_stamp:
             return
+
+        # Convert it to 0000-00-00 format
         dt = str(datetime.fromtimestamp(timestamp)).split(' ')[0]
+
         try:
             title = soup.find('div', class_='c-hero__header').h1.text.strip()
-            print(title)
+            print(f"scrapping {title}")
         except():
+            # Some random pages aren't built well, might be not needed in the future
             print('unknown event name')
 
         fights = soup.find_all('li', class_='l-listing__item')
-        for f in fights:
+        for fight in fights:
             names = list(
                 map(
                     lambda elem: elem.text.strip().replace('\n', '-'),
-                    f.find_all('div', class_='c-listing-fight__corner-name')
+                    fight.find_all('div', class_='c-listing-fight__corner-name')
                 )
             )
             results = list(
-                map(lambda elem: elem.text.strip(), f.find_all('div', class_='c-listing-fight__outcome-wrapper')))
+                map(lambda elem: elem.text.strip(), fight.find_all('div', class_='c-listing-fight__outcome-wrapper')))
             writer.writerow([*names, *results, dt])
 
         next_event = soup.find('a', 'next')["href"]
-        iterate_through_ufc_events(next_event)
+        recurse_through_ufc_events(next_event)
 
 
     with open('fights.csv', 'w', encoding='UTF8') as file:
@@ -40,4 +46,4 @@ if __name__ == "__main__":
         writer = csv.writer(file)
         # Header
         writer.writerow(['first_fighter', 'second_fighter', 'result_1', 'result_2', 'date'])
-        iterate_through_ufc_events()
+        recurse_through_ufc_events()
