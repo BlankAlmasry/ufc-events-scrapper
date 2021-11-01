db87ab2971a5ad586671f3ce867281b402716c2c
blankalmasry <blankhussien@gmail.com>
2021-11-01 21:41:46 +0200

    changed csv headers

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
 import csv
 from datetime import datetime
-import requests
-from bs4 import BeautifulSoup
+import requests as req
 
-base = "https://www.ufc.com"
+"""
+Will Search through Ufc cdn stats and return only ufc events
+Will return the future events result as nothing in csv
+At the time of making this script thee last Scheduled Event had id of 1067
+I FOUND NO PATTERN WHAT SO EVER regarding EventID numbers and anything regarding their corresponding data
+Looks to me stats comes from UFC/DWCS only since event id 819, and it's getting incremented since then
+"""
+
+# Might possibly change in few years
+base = "https://d29dxerjsp82wz.cloudfront.net/api/v3/event/live/"
 
 if __name__ == "__main__":
-    def recurse_through_ufc_events(next_event="/event/ufc-1"):
-        page = requests.get(base + next_event)
-        soup = BeautifulSoup(page.content, "html.parser")
-
-        timestamp = int(soup.find('div', class_='tz-change-inner')['data-timestamp'])
-        # don't scrape the future event
-        if timestamp > current_time_stamp:
+    def iterate_through_ufc_events(event_id):
+        data = req.get(base + str(event_id) + ".json").json()
+        if not data["LiveEventDetail"] or data["LiveEventDetail"]["Organization"]["OrganizationId"] != 1:
+            # Api return empty LiveEventDetail object when event doesn't exist
+            # Ufc Organization id is 1, Dana white contender series is 67, I choose to not include other than ufc
             return
-
-        # Convert it to 0000-00-00 format
-        dt = str(datetime.fromtimestamp(timestamp)).split(' ')[0]
-
-        try:
-            title = soup.find('div', class_='c-hero__header').h1.text.strip()
-            print(title)
-        except():
-            # Some random pages aren't built well, might be not needed in the future
-            print('unknown event name')
-
-        fights = soup.find_all('li', class_='l-listing__item')
-        for fight in fights:
-            names = list(
-                map(
-                    lambda elem: elem.text.strip().replace('\n', '-'),
-                    fight.find_all('div', class_='c-listing-fight__corner-name')
-                )
-            )
-            results = list(
-                map(lambda elem: elem.text.strip(), fight.find_all('div', class_='c-listing-fight__outcome-wrapper')))
-            writer.writerow([*names, *results, dt])
-
-        next_event = soup.find('a', 'next')["href"]
-        recurse_through_ufc_events(next_event)
+        print(f'Scrapping {event_id}  {data["LiveEventDetail"]["Name"]}')
+        date = data["LiveEventDetail"]["StartTime"][:10]
+        for fight in data["LiveEventDetail"]["FightCard"]:
+            fighters = []
+            results = []
+            for index, fighter in enumerate(fight["Fighters"]):
+                fighters.append(fighter["Name"]["FirstName"] + "-" + fighter["Name"]["LastName"])
+                results.append(fighter["Outcome"]["Outcome"])
+            writer.writerow([fighters[0], fighters[1], results[0], results[1], date])
 
 
     with open('fights.csv', 'w', encoding='UTF8', newline='') as file:
         current_time_stamp = int(datetime.now().timestamp())
         writer = csv.writer(file)
         # Header
-        writer.writerow(['first_fighter', 'second_fighter', 'result_1', 'result_2', 'date'])
+        writer.writerow(["fighter1", "fighter2", "result1", "result2", "date"])
         print('started scrapping')
-        recurse_through_ufc_events()
+        """
+        Non UFC Event IDS
+         123~262
+         317~410
+         possibility for them to change in the future is very unlikely,
+         since they use them for their stats site
+        """
+        possible_event_id_values = (num for num in range(1, 1500))
+        for i in possible_event_id_values:
+            iterate_through_ufc_events(i)
